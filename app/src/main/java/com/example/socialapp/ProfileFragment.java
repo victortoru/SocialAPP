e29019db4295072011012bfc81d8d0c84d58b419
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-02-24 17:59:35 +0100

    Seguir con el profile fragment

--- app/src/main/java/com/example/socialapp/ProfileFragment.java
+++ app/src/main/java/com/example/socialapp/ProfileFragment.java
@@ -15,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,8 +24,9 @@ public class ProfileFragment extends Fragment {
 
     NavController navController;   // <-----------------
     ImageView photoImageView;
-    TextView displayNameTextView, emailTextView;
-
+    TextView displayNameTextView, emailTextView, authorTextView, likesTextView;
+    String uid;
+    String nombre;
     public ProfileFragment() {}
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -40,8 +42,24 @@ public class ProfileFragment extends Fragment {
         if(user != null){
             displayNameTextView.setText(user.getDisplayName());
             emailTextView.setText(user.getEmail());
+            Glide.with(requireView())
+                    .load(user.getPhotoUrl())
+                    .transform(new CircleCrop())
+                    .into(photoImageView);
+            uid = user.getUid();
+        }
 
-            Glide.with(requireView()).load(user.getPhotoUrl()).into(photoImageView);
+        if(user.getPhotoUrl() == null){
+            Glide.with(requireView())
+                    .load(R.drawable.anonymo)
+                    .transform(new CircleCrop())
+                    .into(photoImageView);
+
+            String email = emailTextView.getText().toString();
+            int indexArroba = email.indexOf("@");
+            nombre = email.substring(0, indexArroba);
+            displayNameTextView.setText(nombre);
+            uid = user.getUid();
         }
     }
 
